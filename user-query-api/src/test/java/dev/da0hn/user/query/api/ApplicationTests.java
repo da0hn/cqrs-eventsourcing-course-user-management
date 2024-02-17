649bc98d2a9d8796eeb23be79f5e0566a070dcf7
Gabriel Honda <gabrielhondacba@gmail.com>
2024-02-17 11:34:15 -0400

    chore: commit inicial

---
+++ user-query-api/src/test/java/dev/da0hn/user/query/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.da0hn.user.query.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
